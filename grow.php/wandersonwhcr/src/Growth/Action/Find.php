a6cc877dec1f8dd58bbc9d1c897ae27107ac73ca
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-07-15 10:42:40 -0300

    Retrieve stored variable

---
+++ grow.php/wandersonwhcr/src/Growth/Action/Find.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Growth\Action;
+
+class Find
+{
+    protected array $server;
+
+    public function __construct(array $server)
+    {
+        $this->server = $server;
+    }
+
+    public function __invoke(array $args): void
+    {
+        $key = sprintf('%s-%s-%s', $args['country'], $args['indicator'], $args['year']);
+
+        if (! apcu_exists($key)) {
+            header('HTTP/1.1 404 Not Found');
+            return;
+        }
+
+        $data = apcu_fetch($key);
+
+        header('HTTP/1.1 200 OK');
+        header('Content-Type: application/json');
+
+        file_put_contents('php://output', json_encode($data));
+    }
+}
