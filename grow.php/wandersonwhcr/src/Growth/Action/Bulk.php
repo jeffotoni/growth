4870de294c9e182b04afc830ef906cdc0986c259
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-07-15 11:57:07 -0300

    Include a bulk creation

---
+++ grow.php/wandersonwhcr/src/Growth/Action/Bulk.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Growth\Action;
+
+class Bulk
+{
+    protected array $server;
+
+    public function __construct(array $server)
+    {
+        $this->server = $server;
+    }
+
+    protected function getRequestContentType(): ?string
+    {
+        return $this->server['HTTP_CONTENT_TYPE'] ?? null;
+    }
+
+    public function __invoke(): void
+    {
+        if ($this->getRequestContentType() !== 'application/json') {
+            header('HTTP/1.1 415 Unsupported Media Type');
+            return;
+        }
+
+        $dataset = json_decode(file_get_contents('php://input'));
+
+        if (! $dataset) {
+            header('HTTP/1.1 422 Unprocessable Entity');
+            return;
+        }
+
+        if (! is_array($dataset)) {
+            header('HTTP/1.1 422 Unprocessable Entity');
+            return;
+        }
+
+        // TODO Async
+        foreach ($dataset as $data) {
+            $key = sprintf('%s-%s-%s', $data->Country, $data->Indicator, $data->Year);
+
+            apcu_store($key, $data);
+        }
+
+        header('HTTP/1.1 201 Accepted');
+
+        file_put_contents('php://output', json_encode(['msg' => 'In progress']));
+    }
+}
