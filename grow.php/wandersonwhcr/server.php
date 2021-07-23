7b78e42b1c2052eec6cc2d96acd5a232b7b490e0
Wanderson Camargo <wandersonwhcr@gmail.com>
2021-07-23 08:30:27 -0300

    Apresenta informações de inicialização

--- grow.php/wandersonwhcr/server.php
+++ grow.php/wandersonwhcr/server.php
@@ -6,6 +6,10 @@ use Swoole\HTTP\Server;
 
 $server = new Server('0.0.0.0', 8080);
 
+$server->on('Start', function (Server $server) {
+    echo sprintf("[%s] Server started at http://0.0.0.0:8080/\n", date('r'));
+});
+
 $server->on('Request', function (Request $request, Response $response) {
     $response->header('Content-Type', 'application/json');
     $response->end('{"message":"ping"}');
