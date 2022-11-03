b4e047eb6bb16927ca6ee93f49ba976c20199785
jeffotoni <jeff.otoni@gmail.com>
2022-11-03 11:57:36 -0300

    update apis

--- grow.go/jeffotoni/hello/api1/main.go
+++ grow.go/jeffotoni/hello/api1/main.go
@@ -1,17 +1,19 @@
 package main
 
-import "log"
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/hello", Hello)
-	log.Println("Run Server port:8080")
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
 
-func Hello(w http.ResponseWriter, r *http.Request){
+func Hello(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type","application/json")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Add("Engine", "Go")
 	w.Write([]byte(`{"msg":"success"}`))
 	return
 }
